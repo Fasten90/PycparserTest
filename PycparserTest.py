f77c1c98398f396c76d30a0a6d64ab3393f5df2a
Fasten90 <fasten90@gmail.com>
2019-09-21 16:46:01 +0200

    # Return check - pilot

--- PycparserTest.py
+++ PycparserTest.py
@@ -56,6 +56,7 @@ for ast_item in parse_result:
 func_declarations = set()
 func_calls = set()
 goto_used = set()
+return_used = set()
 func_calls_all = []
 
 # Note: be careful, this was child of a pycparser class
@@ -105,6 +106,16 @@ class GotoVisitor(pycparser.c_ast.NodeVisitor):
         goto_used.add(node.name)
 
 
+# Return checker
+class ReturnVisitor(pycparser.c_ast.NodeVisitor):
+    def visit_Return(self, node):
+        # This was called by pycparser NodeVisitor automatically
+        #print("Return used '{}' at '{}'".format(node.name, node.coord))
+
+        global return_used
+        return_used.add(node.coord)
+
+
 checker_obj = FuncCallVisitor()
 checker_obj.visit(parse_result)
 
@@ -114,6 +125,40 @@ checker_obj.visit(parse_result)
 # Goto
 checker_obj = GotoVisitor()
 checker_obj.visit(parse_result)
+
+# Return
+# TODO: Cannot check easily, which function' return
+checker_obj = ReturnVisitor()
+checker_obj.visit(parse_result)
+
+
+def find_return_in_recursive(item_list):
+    return_count = 0
+    if isinstance(item_list, pycparser.c_ast.Return):
+        return 1
+    for item in item_list:
+        return_count += find_return_in_recursive(item)
+    return return_count
+
+
+
+# Explore AST
+for ast_item in parse_result:
+    #print(str(ast_item))
+    #if isinstance(ast_item, list):  # pycparser.c_ast.FuncDecl
+    #if ast_item.storage[0] != "typedef":
+    #if isinstance(ast_item.type, pycparser.c_ast.FuncDecl):
+    #if isinstance(ast_item.type, pycparser.c_ast.FuncDef):
+    if isinstance(ast_item, pycparser.c_ast.FuncDef):
+        # Explorer the body
+        function_name = ast_item.decl.name
+        return_count = 0
+        for body_item in ast_item.body:
+            #print(str(body_item))
+            #if isinstance(body_item, pycparser.c_ast.Return):
+            #    return_count += 1
+            return_count = find_return_in_recursive(body_item)
+        print("Function: '{}' has {} return".format(function_name, return_count))
 
 
 # Listing
